8e9e2bbdefc33d7627ac8e8ef0d63ca3a5f7a910
ashumsd7 <aat8001@gmail.com>
2021-08-17 09:32:58 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+   <span class="badge badge-warning"><h3>Thank you for the downloading the template : BY <a href="http://aashu.online">ASHU</a></h3></span>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
