6eedb5d2be90a15129e09bff55def1b348b5bd21
ashumsd7 <aat8001@gmail.com>
2021-08-21 23:46:23 +0530

    spell chnaged : corrected

--- src/components/boss/TheHeader.vue
+++ src/components/boss/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header>    
-        <img width="200" height="40" src="https://cdn.rivuletdigital.com/rivuletdigital/uploads/2020/07/logo.svg" alt="" srcset=""><strong class="ml-3">Thnak you for the chance.</strong> By : <a href="http://aashu.online">Aashu</a>
+        <img width="200" height="40" src="https://cdn.rivuletdigital.com/rivuletdigital/uploads/2020/07/logo.svg" alt="" srcset=""><strong class="ml-3">Thank you for the chance.</strong> By : <a href="http://aashu.online">Aashu</a>
     </header>
 </template>
 
@@ -12,12 +12,17 @@
 
 <style scoped>
 header{
-    height: 40px;
+    height: auto;
     width: 100vw;
     background: rgb(0, 0, 0);
     color: white;
 }
 strong{
     color: rgb(177, 127, 18);
+}
+@media screen and (max-width:900px) {
+    strong{
+        display: block;
+    }
 }
 </style>
