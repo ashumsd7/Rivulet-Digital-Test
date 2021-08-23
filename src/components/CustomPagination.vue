a00ac73c4949ad7efdd01b481f4cae4960e3d52d
ashumsd7 <aat8001@gmail.com>
2021-08-23 15:38:08 +0530

    building custom pagination component

---
+++ src/components/CustomPagination.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="custom-pagination">
+    <ul class="d-flex justify-content-center flex-wrap">
+        <li>&lt;</li>
+      <li v-for="i in getTotalPage" @click="handlePageAction(i)" :key="i">
+        {{ i }}
+      </li>
+       <li>&gt;</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+   
+    getPageEntry() {
+      return this.$store.getters.getPageEntry;
+    },
+    getTotalPage() {
+      return this.$store.getters.getTotalPage;
+    },
+  },
+  data() {
+    return {};
+  },
+ 
+  methods: {
+    handlePageAction(pageNumber) {
+      this.$store.dispatch("loadComments", {page:pageNumber,entry:this.getPageEntry});
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul li {
+  list-style: none;
+  border: 1px solid black;
+  padding: 0.6rem;
+  background: rgb(226, 217, 217);
+  font-weight: 500;
+}
+ul li:hover {
+  border: 1px solid rgb(64, 216, 5);
+  background: rgb(129, 250, 220);
+}
+ul li.isActive {
+  background: black;
+  color: rgb(28, 255, 7);
+  font-weight: bolder;
+}
+</style>
---
+++ src/components/EntryControl.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-dropdown
+      id="dropdown-left"
+      text="Entry Per Page"
+      variant="primary"
+      class=""
+    >
+      <b-dropdown-item
+        v-for="option in options"
+        @click="handleEntry(option)"
+        :key="option"
+        >{{ option }}</b-dropdown-item
+      >
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    getPageStatus() {
+      return this.$store.getters.getPageStatus;
+    },
+   
+  },
+  data() {
+    return {
+      options: [5,6,7,8,9, 10,11,12,13,14,15,16,17,18,19, 20],
+    };
+  },
+  methods: {
+    handleEntry(option) {
+     
+      this.$store.dispatch("loadComments", {page:1, entry:option});
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
